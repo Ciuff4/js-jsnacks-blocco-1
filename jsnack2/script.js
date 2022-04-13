34bb922df8d6116917a0355667e304b17268ffe5
Ciuff4 <gasperini_cristina@libero.it>
2022-04-13 11:05:38 +0200

    added some style css. fixed prints with shortest/longest word

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -6,12 +6,32 @@
 
 let firstWord= prompt("Insert the first word:");
 let secondWord= prompt("Insert the second word:");
+const longest= document.getElementById("longest-word");
+const shortest=document.getElementById("shortest-word") 
 
 if (firstWord.length===secondWord.length) {
     console.log("tie");
+    longest.innerHTML= `
+    <p> the word length is the same </p>
+        `;
+        shortest.innerHTML= `
+    <p> the word length is the same </p>
+        `;
 }else if(firstWord.length>secondWord.length){
     console.log("first word");
+    longest.innerHTML= `
+    <p>The longest word is: </p> ${firstWord}
+        `;
+        shortest.innerHTML= `
+    <p>The shortest word is: </p> ${secondWord}
+        `;
 }else{
+    longest.innerHTML= `
+    <p>The longest word is: </p> ${secondWord}
+        `;
+    shortest.innerHTML= `
+    <p>The shortest word is: </p> ${firstWord}
+        `;
     console.log("second word");
 }
 
